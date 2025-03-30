5475bf478c46314f99f008d1e58d66f241d5d091
jgiambona <jason.giambona@gmail.com>
2025-03-30 16:31:57 +0800

    Update formula to v1.4.1

--- Formula/fancy-tar.rb
+++ Formula/fancy-tar.rb
@@ -1,11 +1,16 @@
 class FancyTar < Formula
-  desc "Tar with progress, file info, and notifications"
+  desc "Tar with progress, file info, encryption, and hash verification"
   homepage "https://github.com/jgiambona/fancy-tar"
-  url "https://github.com/jgiambona/fancy-tar/releases/download/v1.4.1/fancy-tar-1.4.1.tar.gz"
-  sha256 "e60f75166bc58b83bf6a9663d3084ec558eddb2707ead351039a351e638d0854"
+  url "https://github.com/jgiambona/fancy-tar/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "__REPLACE_ME__"
   license "MIT"
 
   depends_on "pv"
+  depends_on "gnu-tar"
+  depends_on "coreutils"
+  depends_on "gpg" => :optional
+  depends_on "openssl@3" => :optional
+  depends_on "p7zip" => :optional
 
   def install
     bin.install "scripts/fancy_tar_progress.sh" => "fancy-tar"
@@ -16,6 +21,6 @@ class FancyTar < Formula
   end
 
   test do
-    system "#<built-in function bin>/fancy-tar", "-h"
+    assert_match "fancy-tar", shell_output("#{bin}/fancy-tar --version")
   end
 end
